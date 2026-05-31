22c64dfe7da7152641ecfe5037a3fb6914f43afc
dunnand <55661320+dunnand@users.noreply.github.com>
2026-05-30 22:10:07 -0400

    Update BSI now-playing output

--- README.md
+++
@@ -1,2 +0,0 @@
-# WCYT-Website
-Website for The Point 91.1 FM WCYT
--- bsi_watcher_svc.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Users\Andy\AppData\Local\Programs\Python\Python314\python.exe" "C:\Users\Andy\WCYT-Website\bsi_watcher.py"
